fecb91871bb5b0a3f9c8cbe5bb469139625d701c
Alex <alexhuynh1999@gmail.com>
2019-08-08 00:21:58 -0500

    testing API calling

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tweepy
-#this project was mainly to test calling an API 
+#this project was mainly to test calling an API
 # Authenticate to Twitter
 auth = tweepy.OAuthHandler("SHvhBU7B0meKLTsR4IcLeBVhq", "Y1ddnvAlhxJ1GsPkjdL0MhYATfnpU9FHE9rlhbCgJgg8iPzMDm")
 auth.set_access_token("1155451352-ypbhuYDq3tTNGUze2L1U268P0r2yyDyYXRWSYJL", "kP4zdXSSsfTBp1rZ84GIChMEi8iWYTnOSkx2fpiH99z99")
