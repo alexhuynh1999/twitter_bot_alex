0cc560618088e6383c4a9792f3c22a719d955ddc
Alex <alexhuynh1999@gmail.com>
2019-08-08 00:16:17 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import tweepy
+
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler("SHvhBU7B0meKLTsR4IcLeBVhq", "Y1ddnvAlhxJ1GsPkjdL0MhYATfnpU9FHE9rlhbCgJgg8iPzMDm")
+auth.set_access_token("1155451352-ypbhuYDq3tTNGUze2L1U268P0r2yyDyYXRWSYJL", "kP4zdXSSsfTBp1rZ84GIChMEi8iWYTnOSkx2fpiH99z99")
+
+# Create API Object
+api = tweepy.API(auth)
+
+# check to authenticate the keys
+# try:
+#     api.verify_credentials()
+#     print("Authentication OK")
+# except:
+#     print("Error during authentication")
+
+#create a tweet
+#api.update_status("Hello World")
+
